95dcbe0e43eb5e9a1c492dc4ddee2450ae5cc576
5rshv <serishev.alesha@gmail.com>
2024-12-03 22:29:39 +0300

    git commit --grade increased complexity

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,14 @@ public class Main {
         System.out.println(sum());
         System.out.println(srSum());
         printSotr();
+        indexZp(2);
+        infoOtdel(5);
+        sotrOtdelSum(2);
+        sotrOtdelSrZnachSum(2);
+        sotrOtdeIndexZp(2, 5);
+        sotrOtde(2);
+        sotrMinZp(16000);
+        sotrMaxZp(38434);
     }
 
     public static void print() {
@@ -69,6 +77,122 @@ public class Main {
         System.out.println("сотрудники: ");
         for (int i = 0; i < employee.length; i++) {
             System.out.println(employee[i].getFio());
+        }
+    }
+
+    public static void indexZp(int percent) {
+        System.out.println("Индексация зарплаты у сотрудников на 5%: ");
+        for (int i = 0; i < employee.length; i++) {
+            double indexZp = employee[i].getZarplata();
+            indexZp = (indexZp * (percent / 100)) + indexZp;
+            System.out.println(Math.round(indexZp));
+        }
+    }
+
+    public static void infoOtdel(int otdel) {
+        sotrOtdelMinZp(otdel);
+        sotrOtdelMaxZp(otdel);
+    }
+
+
+    public static void sotrOtdelMinZp(int otdel) {
+        System.out.println("Человек с минимальной ЗП в отделе: ");
+        double minZp = Integer.MAX_VALUE;
+        for (int i = 0; employee.length > i; i++) {
+            if (employee[i].getOtdel() != otdel || employee[i] == null) {
+                continue;
+            }
+            if (minZp > employee[i].getZarplata()) {
+                minZp = employee[i].getZarplata();
+            }
+        }
+        System.out.println(minZp);
+    }
+
+
+    public static void sotrOtdelMaxZp(int otdel) {
+        System.out.println("Человек с максимальной ЗП в отделе: ");
+        double maxZp = -1;
+        for (int i = 0; employee.length > i; i++) {
+            if (employee[i].getOtdel() != otdel || employee[i] == null) {
+                continue;
+            }
+            if (maxZp < employee[i].getZarplata()) {
+                maxZp = employee[i].getZarplata();
+            }
+        }
+        System.out.println(maxZp);
+    }
+
+    public static void sotrOtdelSum(int otdel) {
+        System.out.println("Сумму затрат на зп по отделу: ");
+        double sum = 0;
+        for (int i = 0; employee.length > i; i++) {
+            if (employee[i].getOtdel() != otdel || employee[i] == null) {
+                continue;
+            }
+            sum = sum + employee[i].getZarplata();
+        }
+        System.out.println(Math.round(sum));
+    }
+
+    public static void sotrOtdelSrZnachSum(int otdel) {
+        System.out.println("Средняя зп по отделу: ");
+        double srSum = 0;
+        int a = 0;
+        for (int i = 0; employee.length > i; i++) {
+            if (employee[i].getOtdel() != otdel || employee[i] == null) {
+                continue;
+            }
+            a++;
+            srSum = srSum + employee[i].getZarplata();
+        }
+        System.out.println(Math.round(srSum / a));
+    }
+
+
+    public static void sotrOtdeIndexZp(int otdel, double percent) {
+        System.out.println("Индексация зп по отделу: ");
+        double indexZp;
+        for (int i = 0; employee.length > i; i++) {
+            if (employee[i].getOtdel() != otdel || employee[i] == null) {
+                continue;
+            }
+            indexZp = employee[i].getZarplata();
+            indexZp = (indexZp * (percent / 100)) + indexZp;
+            System.out.println(Math.round(indexZp));
+        }
+    }
+
+    public static void sotrOtde(int otdel) {
+        System.out.println("Сотрудники отдела: ");
+        for (int i = 0; employee.length > i; i++) {
+            if (employee[i].getOtdel() != otdel || employee[i] == null) {
+                continue;
+            }
+            System.out.print(employee[i].getFio() + " " + employee[i].getZarplata() + " " + employee[i].getId());
+            System.out.println();
+        }
+    }
+
+    public static void sotrMinZp(double number) {
+        System.out.println("ЗП меньше числа: ");
+        for (int i = 0; employee.length > i; i++) {
+            if (number > employee[i].getZarplata()) {
+                System.out.print(employee[i].getId() + " " + employee[i].getFio() + " " + employee[i].getZarplata());
+                System.out.println();
+            }
+
+        }
+    }
+
+    public static void sotrMaxZp(double number) {
+        System.out.println("ЗП больше или равно числа: ");
+        for (int i = 0; employee.length > i; i++) {
+            if (number <= employee[i].getZarplata()) {
+                System.out.print(employee[i].getId() + " " + employee[i].getFio() + " " + employee[i].getZarplata());
+                System.out.println();
+            }
         }
     }
 }
